4fb82be76c50fcbce0006a77a906e51d19569850
MokhlisBelhaj <belhajmokhlis0@gmail.com>
2022-09-12 17:21:19 +0100

    la table de multiplicationde 2

--- la table de multiplicationde.c
+++ la table de multiplicationde.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
 void tmultiplication(int a){
-	int i,n,c=0;
+	int i,n,c=0,j,D=0;
 	printf("la table de multiplications de %d est : \n",a);
-	for(i = 0 ;i<=10;i++){
-      printf("%d x %d = %d \n ",a,i,a*i);
-      
+	for(i = 1 ;i<=10;i++){
+      printf("%d x %d = %d \n ",a,i,a*i); 
 
 	}
 	printf("------------------------------------------------\n");
 	printf("veuillez entrer la denominateur:\n");
 	scanf("%d",&n);
-	for(i = 0; i<=10; i++){
-		if(a*i % n == 0&&a*i!=0){
-			printf("%d x %d = %d \n ",a,i,a*i);
-		c++;
-		}else{
-			i++;
+	
+	
+
+	
+	for(j = 1; j<=10; j++){
+		if((a*j) % n == 0){
+			printf("%d \n ",a*j);
+		  c++;
 		}
-		
-		
 	}
 	if(c==0){
 		printf("aucun\n");
@@ -28,12 +28,18 @@ void tmultiplication(int a){
 
 int main()
 {
-	start:;
-    int N;
+	int N,d;
+	do {
+	   
          printf("veuillez entrer la valeur de N:");
          scanf("%d",&N);
          tmultiplication(N);
-         goto start;
+		printf("pour continer taper 1 \npour quiter taper 0 \n");	
+		scanf("%d",&d);
+		
+	}while(d==1);
+   
+         printf( "bye");
     
      return 0;
 }
