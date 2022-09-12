1f28d07f22a970fd645809c98634cd29e410b029
MokhlisBelhaj <belhajmokhlis0@gmail.com>
2022-09-12 16:00:27 +0100

    la table de multiplicationde 1

--- la table de multiplicationde.c
+++ la table de multiplicationde.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void tmultiplication(int a){
-	int i,n;
-	printf("la table de multiplicationde % est : \n",a);
+	int i,n,c=0;
+	printf("la table de multiplications de %d est : \n",a);
 	for(i = 0 ;i<=10;i++){
       printf("%d x %d = %d \n ",a,i,a*i);
       
@@ -13,15 +13,27 @@ void tmultiplication(int a){
 	for(i = 0; i<=10; i++){
 		if(a*i % n == 0&&a*i!=0){
 			printf("%d x %d = %d \n ",a,i,a*i);
+		c++;
+		}else{
+			i++;
 		}
+		
+		
 	}
+	if(c==0){
+		printf("aucun\n");
+	} 
+			
 }
+
 int main()
 {
+	start:;
     int N;
          printf("veuillez entrer la valeur de N:");
          scanf("%d",&N);
          tmultiplication(N);
+         goto start;
     
      return 0;
 }
